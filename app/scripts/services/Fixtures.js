c1d617694074f945cbf034c7087814788e3bc3a1
Muhammad Javed <daudjaved1@gmail.com>
2017-05-02 12:35:13 -0400

    assignment 22 completed

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -29,10 +29,17 @@
              { title: 'Can you hear me now?', duration: '3:14' },
              { title: 'Wrong phone number', duration: '2:15' }
          ]
-     };
+         };
          Fixtures.getAlbum = function() {
             return albumPicasso;
-     };
+         };
+         Fixtures.getCollection = function(numberOfAlbums){
+             var arr = [] ;
+             for(var i = 0; i <= numberOfAlbums ; i++){
+                 arr.push(albumPicasso);
+             }
+             return arr;
+         };
          return Fixtures;
      }
  
