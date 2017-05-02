21c211e098125bfa553d14d3523ef73dd6e11bc6
Muhammad Javed <daudjaved1@gmail.com>
2017-05-02 17:32:26 -0400

    completed assignment 23

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,10 @@
  (function() {
     function SongPlayer() {
         var SongPlayer = {};
-
+/**
+ * @desc current song 
+ * @type {Object}
+ */
         var currentSong = null;
  /**
  * @desc Buzz object audio file
@@ -26,11 +29,21 @@
  
         currentSong = song;
     };
+    /**
+ * @function playSong
+ * @desc Plays the current buzz object 
+ * @param {Object} song
+ */
+    var playSong = function(song){
+        currentBuzzObject.play();
+        song.playing = true;
+        
+    };
+   
      SongPlayer.play = function(song) {
          if (currentSong !== song) {
              setSong(song);
-             currentBuzzObject.play();
-             song.playing = true;
+             playSong(song);
               } else if (currentSong === song) {
                     if (currentBuzzObject.isPaused()) {
                         currentBuzzObject.play();
